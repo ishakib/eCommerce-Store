012faddb767f3c9eb3684526838f219ebbaff777
Shaki hossain <akm.shakib@gmail.com>
2023-10-28 06:38:56 +0600

    cmd for create all entities

--- app/Console/Commands/GenerateEntities.php
+++ app/Console/Commands/GenerateEntities.php
@@ -15,36 +15,38 @@ class GenerateEntities extends Command
         $entityData = $this->getDatabaseStructure();
 
         foreach ($entityData as $entityName => $columns) {
-            $migrationName = 'create_' . Str::snake($entityName) . '_table';
             $baseName = Str::singular(Str::studly($entityName));
-
-            $options = [
-                'name' => $baseName,
-                '--migration' => true,
-            ];
-
+            $migrationName = 'create_' . $entityName . '_table';
             $this->call('make:migration', [
                 'name' => $migrationName,
-                '--create' => Str::snake($entityName),
+                '--create' => $entityName,
+            ]);
+            $this->addColumnsToMigration($migrationName, $columns);
+
+            // Create model, controller, request, and resource
+            $modelName = Str::singular(Str::studly($entityName));
+            $this->call('make:model', [
+                'name' => $modelName,
+                '--migration' => true,
             ]);
 
-            // Create model
-            $this->call('make:model', $options);
+            // Add fillable property to the generated model
+            $this->addFillablePropertyToModel($baseName, array_keys($columns));
 
-            // Create controller
+            $controllerName = $modelName . 'Controller';
             $this->call('make:controller', [
-                'name' => $baseName . 'Controller',
-                '--model' => $baseName,
+                'name' => $controllerName,
+                '--model' => $modelName,
             ]);
 
-            // Create request
+            $requestName = $modelName . 'Request';
             $this->call('make:request', [
-                'name' => $baseName . 'Request',
+                'name' => $requestName,
             ]);
 
-            // Generate API resource routes for both API and web
+            $resourceName = $modelName . 'Resource';
             $this->call('make:resource', [
-                'name' => $baseName . 'Resource',
+                'name' => $resourceName,
             ]);
 
             // Output success message
@@ -52,192 +54,67 @@ class GenerateEntities extends Command
         }
     }
 
-    private function addApiResourceRoute($filename, $entityName)
+    private function addColumnsToMigration($migrationName, $columns)
     {
-        $pluralEntityName = Str::plural($entityName);
-        $controllerName = $entityName . 'Controller';
+        $migrationFileName = $this->getMigrationFileName($migrationName);
+        $migrationPath = database_path('migrations') . '/' . $migrationFileName;
 
-        // Determine the target file (api.php or web.php)
-        $filePath = $filename === 'api.php' ? 'routes/api.php' : 'routes/web.php';
-
-        // Add an API resource route to the specified file
-        $this->appendFile(
-            base_path($filePath),
-            "\nRoute::resource('$pluralEntityName', $controllerName);"
-        );
-    }
-
-    private function appendFile($path, $content)
-    {
-        file_put_contents($path, file_get_contents($path) . $content);
-    }
-
-    public function getDatabaseStructure(): array
-    {
-        $entityData = [
-            'users' => [
-                'id' => 'INT',
-                'name' => 'VARCHAR',
-                'email' => 'VARCHAR',
-                'password' => 'VARCHAR (hashed)',
-                'registration_date' => 'DATETIME',
-                'last_login' => 'DATETIME',
-                'phone' => 'VARCHAR',
-                'role' => 'ENUM ("Customer", "Admin")',
-            ],
-            'addresses' => [
-                'id' => 'INT',
-                'user_id' => 'INT (Foreign Key)',
-                'street_address' => 'VARCHAR',
-                'city' => 'VARCHAR',
-                'state' => 'VARCHAR',
-                'postal_code' => 'VARCHAR',
-                'country' => 'VARCHAR',
-            ],
-            'categories' => [
-                'id' => 'INT',
-                'name' => 'VARCHAR',
-            ],
-            'products' => [
-                'id' => 'INT',
-                'name' => 'VARCHAR',
-                'description' => 'TEXT',
-                'price' => 'DECIMAL',
-                'stock_quantity' => 'INT',
-                'manufacturer' => 'VARCHAR',
-                'category_id' => 'INT (Foreign Key)',
-            ],
-            'product_images' => [
-                'id' => 'INT',
-                'product_id' => 'INT (Foreign Key)',
-                'image_url' => 'VARCHAR',
-            ],
-            'orders' => [
-                'id' => 'INT',
-                'user_id' => 'INT (Foreign Key)',
-                'order_date' => 'DATETIME',
-                'status' => 'ENUM ("Cart", "Pending", "Shipped", "Delivered")',
-                'total_amount' => 'DECIMAL',
-                'shipping_address_id' => 'INT (Foreign Key)',
-                'billing_address_id' => 'INT (Foreign Key)',
-                'payment_method' => 'VARCHAR',
-                'payment_status' => 'ENUM ("Pending", "Completed", "Failed")',
-            ],
-            'order_items' => [
-                'id' => 'INT',
-                'order_id' => 'INT (Foreign Key)',
-                'product_id' => 'INT (Foreign Key)',
-                'quantity' => 'INT',
-                'price' => 'DECIMAL',
-                'subtotal' => 'DECIMAL',
-            ],
-            'reviews' => [
-                'id' => 'INT',
-                'user_id' => 'INT (Foreign Key)',
-                'product_id' => 'INT (Foreign Key)',
-                'rating' => 'INT',
-                'comment' => 'TEXT',
-                'created_at' => 'DATETIME',
-            ],
-            'payments' => [
-                'id' => 'INT',
-                'order_id' => 'INT (Foreign Key)',
-                'amount' => 'DECIMAL',
-                'payment_date' => 'DATETIME',
-                'payment_method' => 'VARCHAR',
-                'payment_status' => 'ENUM ("Pending", "Completed", "Failed")',
-            ],
-            'admins' => [
-                'id' => 'INT',
-                'username' => 'VARCHAR',
-                'password' => 'VARCHAR (hashed)',
-                'email' => 'VARCHAR',
-                'profile_picture' => 'VARCHAR',
-            ],
-            'inventories' => [
-                'id' => 'INT',
-                'product_id' => 'INT (Foreign Key)',
-                'stock_quantity' => 'INT',
-                'restock_threshold' => 'INT',
-            ],
-            'promotions' => [
-                'id' => 'INT',
-                'code' => 'VARCHAR',
-                'description' => 'TEXT',
-                'discount_type' => 'ENUM ("Percentage", "Fixed")',
-                'discount_amount' => 'DECIMAL',
-                'start_date' => 'DATETIME',
-                'end_date' => 'DATETIME',
-                'min_purchase_amount' => 'DECIMAL',
-            ],
-            'carts' => [
-                'id' => 'INT',
-                'user_id' => 'INT (Foreign Key)',
-                'product_id' => 'INT (Foreign Key)',
-                'quantity' => 'INT',
-            ],
-            'category_hierarchies' => [
-            ],
-            'attributes' => [
-                'id' => 'INT',
-                'name' => 'VARCHAR',
-                'description' => 'TEXT',
-            ],
-            'variant_attributes' => [
-                'id' => 'INT',
-                'attribute_id' => 'INT (Foreign Key)',
-                'name' => 'VARCHAR',
-                'description' => 'TEXT',
-            ],
-            'brands' => [
-                'id' => 'INT',
-                'name' => 'VARCHAR',
-                'description' => 'TEXT',
-            ],
-            'units' => [
-                'id' => 'INT',
-                'name' => 'VARCHAR',
-                'abbreviation' => 'VARCHAR',
-                'description' => 'TEXT',
-            ],
-        ];
-
-        return $this->convertLaravelMigration($entityData);
-    }
-
-    public function convertLaravelMigration($entityData): array
-    {
-        $laravelEntityData = [];
-
-        foreach ($entityData as $entityName => $columns) {
-            $laravelColumns = [];
-
-            foreach ($columns as $columnName => $columnType) {
-                // Convert column name to snake_case and replace spaces with underscores
-                $laravelColumnName = Str::snake($columnName);
-
-                // Convert column type to Laravel data type
-                $laravelColumnType = match (true) {
-                    str_contains($columnType, 'INT') => 'integer',
-                    str_contains($columnType, 'VARCHAR') => 'string',
-                    str_contains($columnType, 'TEXT') => 'text',
-                    str_contains($columnType, 'DATETIME') => 'dateTime',
-                    default => 'string',
-                };
-
-                // Check for 'Foreign Key' and set 'foreign' attribute if found
-                $foreign = str_contains($columnType, 'Foreign Key');
-
-                // Add the column to the Laravel columns array
-                $laravelColumns[$laravelColumnName] = [
-                    'type' => $laravelColumnType,
-                    'foreign' => $foreign,
-                ];
-            }
-
-            $laravelEntityData[$entityName] = $laravelColumns;
+        if (!file_exists($migrationPath)) {
+            $this->error("Migration file not found: $migrationPath");
+            return;
         }
 
-        return $laravelEntityData;
+        // Read the migration file contents
+        $contents = file_get_contents($migrationPath);
+
+        // Find the position to insert column definitions
+        $pos = strpos($contents, '$table->id();');
+        if ($pos === false) {
+            return; // Handle the case where '$table->id();' is not found
+        }
+
+        // Initialize the column definitions
+        $columnDefinitions = '';
+        foreach ($columns as $columnName => $columnType) {
+            $columnDefinitions .= "\n\t\t\t\$table->$columnType('$columnName');";
+        }
+
+        // Insert the generated column definitions into the migration file
+        $contents = substr_replace($contents, $columnDefinitions, $pos + 13, 0);
+
+        // Write the updated contents back to the migration file
+        file_put_contents($migrationPath, $contents);
+    }
+
+    private function getMigrationFileName($migrationName): string
+    {
+        $timestamp = now()->format('Y_m_d_His');
+        return $timestamp . '_' . $migrationName . '.php';
+    }
+
+    private function addFillablePropertyToModel($modelName, $columns)
+    {
+        $fillableProperty = "protected \$fillable = [\n    '" . implode("',\n    '", $columns) . "'\n];";
+
+        $modelPath = app_path("Models/{$modelName}.php");
+        $modelContent = file_get_contents($modelPath);
+
+        // Replace or add the fillable property
+        $modelContent = preg_replace('/protected \$fillable = \[.*\];/s', $fillableProperty, $modelContent, 1);
+
+        file_put_contents($modelPath, $modelContent);
+    }
+
+    private function getDatabaseStructure(): array
+    {
+        return [
+            'users' => [
+                'name' => 'string',
+            ],
+            'addresses' => [
+                'user_id' => 'foreignId',
+                'street_address' => 'string',
+            ],
+        ];
     }
 }
